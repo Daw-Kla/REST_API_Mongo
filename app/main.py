e1bb7a977421d7d721fdad9fe84b10dab66e6ee0
Daw-Kla <dklassa@wp.pl>
2024-02-04 19:45:28 +0100

    small changes

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from flask import Flask, Blueprint
 from config import Config
 from routes import register_routes
 from database import init_app
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = db = init_app(app)
+db = init_app(app)
 
 # API Blueprint
 api_bp = Blueprint('api', __name__)
@@ -15,4 +16,6 @@ app.register_blueprint(api_bp, url_prefix='/api')
 register_routes(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+    app.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
